17316c67d6aa2cb8f648e6af5c9fc4b020d9c69a
CafeComLag <nicholas.saraivadev@gmail.com>
2025-08-31 16:24:21 +0100

    FEAT: Redirecting and Reciving files.

--- main.c
+++ main.c
@@ -16,20 +16,24 @@
 char *get_command(char *arg, char **paths);
 char **get_paths(char **env);
 int init_pid(pid_t *pid);
-int    reciver_fork(char *argv, char **env, int fd[2]);
-int    giver_fork(char *argv, char **env, int fd[2]);
+int    reciver_fork(char *argv, char **env, int fd[2], int fd_file[2]);
+int giver_fork(char *argv, char **env, int fd[2], int fd_file[2]);
+int get_fd_file(int fd_file[2], char *read_file, char *write_file);
 
 int main(int argc, char *argv[], char *env[])
 {
+    int     fd_file[2];
     int     fd[2];
 
     if (pipe(fd) == -1)
         return (1);
     if (argc < 2)
         return (1);
-    if (!giver_fork(argv[1], env, fd))
+    if (!get_fd_file(fd_file, argv[1], argv[4]))
         return (1);
-    if (!reciver_fork(argv[2], env, fd))
+    if (!giver_fork(argv[2], env, fd, fd_file))
+        return (1);
+    if (!reciver_fork(argv[3], env, fd, fd_file))
         return (1);
     close(fd[0]);
     close(fd[1]);
@@ -37,10 +41,24 @@ int main(int argc, char *argv[], char *env[])
     return (0);
 }
 
-int    giver_fork(char *argv, char **env, int fd[2])
+int get_fd_file(int fd_file[2], char *read_file, char *write_file)
+{
+    if (access(read_file, R_OK) != 0 || access(write_file, W_OK) != 0)
+        return (0);
+    fd_file[0] = open(read_file, O_RDONLY);
+    if (fd_file[0] == -1)
+        return (0);
+    fd_file[1] = open(write_file, O_WRONLY);
+    if (fd_file[1] == -1)
+        return (0);
+    return (1);
+}
+
+int giver_fork(char *argv, char **env, int fd[2], int fd_file[2])
 {
     char    *cmd;
     char    **args;
+    int     new_fd;
     pid_t   pid;
 
     if (!init_pid(&pid))
@@ -49,9 +67,12 @@ int    giver_fork(char *argv, char **env, int fd[2])
     cmd = get_command(args[0], env);
     if (pid == 0)
     {
+        dup2(fd_file[0], STDIN_FILENO);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
         close(fd[0]);
+        close(fd_file[1]);
+        close(fd_file[0]);
         if (cmd)
             execve(cmd, args, env);
         else
@@ -64,7 +85,7 @@ int    giver_fork(char *argv, char **env, int fd[2])
     return (1);
 }
 
-int    reciver_fork(char *argv, char **env, int fd[2])
+int    reciver_fork(char *argv, char **env, int fd[2], int fd_file[2])
 {
     char    *cmd;
     char    **args;
@@ -76,9 +97,12 @@ int    reciver_fork(char *argv, char **env, int fd[2])
     cmd = get_command(args[0], env);
     if (pid == 0)
     {
+        dup2(fd_file[1], STDOUT_FILENO);
         dup2(fd[0], STDIN_FILENO);
         close(fd[1]);
         close(fd[0]);
+        close(fd_file[1]);
+        close(fd_file[0]);
         if (cmd)
             execve(cmd, args, env);
         else
